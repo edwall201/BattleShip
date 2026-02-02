51b9ca01f62c985b095c83c9379b49fdfd6698e6
Edward Huang <yh475@duke.edu>
2026-02-02 15:01:01 -0500

    finish goal1

--- app/src/main/java/edu/duke/yh475/battleship/App.java
+++ app/src/main/java/edu/duke/yh475/battleship/App.java
@@ -3,12 +3,44 @@
  */
 package edu.duke.yh475.battleship;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.Reader;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+public class App {
+  final Board<Character> theBoard;
+  final BoardTextView view;
+  final BufferedReader inputReader;
+  final PrintStream out;
+
+  public App(Board<Character> theBoard, Reader inputSource, PrintStream out){
+    this.theBoard = theBoard;
+    this.view = new BoardTextView(theBoard);
+    this.inputReader = new BufferedReader(inputSource);
+    this.out = out;
+  }
+
+  public Placement readPlacement(String prompt) throws IOException{
+    out.println(prompt);
+    String s = inputReader.readLine();
+    return new Placement(s);
+  }
+
+  public void doOnePlacement() throws IOException{
+    Placement p = readPlacement("where would you like to put your ship? ");
+    Ship<Character> s = new BasicShip(p.getWhere());
+    theBoard.tryAddShip(s);
+    out.print(view.displayMyOwnBoard());
+  }
+
+  public static void main(String [] arts) throws IOException{
+    BattleShipBoard<Character> b = new BattleShipBoard<>(10, 20);
+    App app = new App(b, new InputStreamReader(System.in), System.out);
+    app.doOnePlacement();
+    	
+    
+  }
+  
 }
--- app/src/test/java/edu/duke/yh475/battleship/AppTest.java
+++ app/src/test/java/edu/duke/yh475/battleship/AppTest.java
@@ -3,12 +3,71 @@
  */
 package edu.duke.yh475.battleship;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.StringReader;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.parallel.ResourceAccessMode;
+import org.junit.jupiter.api.parallel.ResourceLock;
+import  org.junit.jupiter.api.parallel.Resources;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+     @Test
+     void test_read_placement() throws IOException{
+       StringReader sr = new StringReader("B2V\nC8H\na4v\n");
+
+       ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+       PrintStream ps = new PrintStream(bytes, true);
+       Board<Character> b = new BattleShipBoard<>(10, 20);
+       App app = new App(b, sr, ps);
+
+       String prompt = "Please enter a location for a ship:";
+       Placement[] expected = new Placement[3];
+       expected[0]  = new Placement(new Coordinate(1,2), 'V');
+       expected[1] = new Placement(new Coordinate(2,8), 'H');
+       expected[2] = new Placement(new Coordinate(0,4), 'V');
+
+       for(int i = 0; i < expected.length; i ++){
+         Placement p = app.readPlacement(prompt);
+         assertEquals(p, expected[i]);
+         assertEquals(prompt + "\n", bytes.toString());
+         bytes.reset();
+       }
+     }
+
+  @Test
+  @ResourceLock(value = Resources.SYSTEM_OUT, mode = ResourceAccessMode.READ_WRITE)
+  void test_main() throws IOException{
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    PrintStream out = new PrintStream(bytes, true);
+
+    InputStream input = getClass().getClassLoader().getResourceAsStream("input.txt");
+    InputStream expectedStream = getClass().getClassLoader().getResourceAsStream("output.txt");
+    assertNotNull(input);
+
+    InputStream oldIn = System.in;
+    PrintStream oldOut = System.out;
+
+    try{
+      System.setIn(input);
+      System.setOut(out);
+      App.main(new String[0]);
     }
+    finally{
+      System.setIn(oldIn);
+      System.setOut(oldOut);
+    }
+
+    String expected = new String(expectedStream.readAllBytes());
+    String actual = bytes.toString();
+    assertEquals(expected, actual);
+  }
+
+  
 }
