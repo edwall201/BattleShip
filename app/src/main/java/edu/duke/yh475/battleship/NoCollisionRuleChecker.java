b4da5fbba3bb82ef544dc25335ea2678de3ac9eb
Edward Huang <yh475@duke.edu>
2026-02-07 21:03:36 -0500

    update task 14 document

--- app/src/main/java/edu/duke/yh475/battleship/NoCollisionRuleChecker.java
+++ app/src/main/java/edu/duke/yh475/battleship/NoCollisionRuleChecker.java
@@ -1,9 +1,22 @@
 package edu.duke.yh475.battleship;
 
+
+/**
+ * A rule checker that ensures the coordinate didn't be occupied
+ */
 public class NoCollisionRuleChecker<T> extends PlacementRuleChecker<T> {
+
+  /**
+   * Constructs a NoCollisionRuleChecker with a reference to next rules
+   *@param is the next rule in teh chain
+  */
   public NoCollisionRuleChecker(PlacementRuleChecker<T> next) {
     super(next);
   }
+
+  /**
+   * Checks if the given ship's coordinate has collosision with other ships on the board or not
+   */
 
   @Override
   protected boolean checkMyRule(Ship<T> theShip, Board<T> theBoard) {
